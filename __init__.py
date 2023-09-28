5a728fc20482a0e7f4e57170064099e8ea292a3b
Will Pisani <wapisani@mtu.edu>
2023-09-28 16:33:49 -0500

    Decided to put all generators in init script

--- __init__.py
+++ __init__.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 """
+
+@author: Dr. William A. Pisani
+
 ElasticMD
 ========
 
-A Python package to enable easier analysis of stress-strain curves produced
+A Python package to enable easy analysis of stress-strain curves produced
 from LAMMPS MD simulations.
 
-Provides (work-in-progress)
-    1. A class to extract thermodynamic data from LAMMPS log files
-    2. A function to export user-specified data as CSV files
-    3. Functions to analyze bulk modulus simulations
-    4. Functions to analyze stress-strain data and produce elastic moduli
+Provides
+    1. A class to extract and export thermodynamic data from LAMMPS log files
+    2. Functions to analyze bulk modulus simulations
+    3. Functions to analyze stress-strain data and produce elastic moduli
        including Young's modulus, Poisson's ratio, and shear modulus
-    5. GUIs to visually analyze stress-strain data, the recommended way
+    4. GUIs to visually analyze stress-strain data, the recommended way
+    5. Functions to generate LAMMPS input scripts for isotropic compression (bulk modulus), uniaxial tension (elastic modulus, Poisson's ratio), and shear (shear modulus)
 
-@author: William A. Pisani, Ph.D.
 """
 
 # Import modules
 from . import extract
-from . import export
 from . import analysis
+from . import generate
